425f56caac5d6edcedd6509c54a4755603a90082
Анастасия Пуха <pukha_an@mail.ru>
2024-02-03 21:15:16 +0300

    added page transitions

--- src/main/java/com/exampleblog/firstblog/controllers/MainController.java
+++ src/main/java/com/exampleblog/firstblog/controllers/MainController.java
@@ -16,7 +16,7 @@ public class MainController {
 
     @GetMapping("/about")
     public String about(Model model) {
-        model.addAttribute("title", "Про нас");
+        model.addAttribute("title", "Страница про нас");
 		return "about";
     }
 }
