b79bda1a48f590a248e3df19f7c4ebd2d8a98726
Анастасия Пуха <pukha_an@mail.ru>
2024-02-03 20:41:28 +0300

    designed the home page template

--- src/main/java/com/exampleblog/firstblog/controllers/MainController.java
+++ src/main/java/com/exampleblog/firstblog/controllers/MainController.java
@@ -1,14 +1,22 @@
 package com.exampleblog.firstblog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
     final String HW = "Hello, World!";
 
     @GetMapping("/")
-    public String returnHelloWorld() {
-        return HW;
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+		return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "Про нас");
+		return "about";
     }
 }
