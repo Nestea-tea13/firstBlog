69dd8c3cea704edabbdf371022aab5c7ec3b34f2
Анастасия Пуха <pukha_an@mail.ru>
2024-02-06 13:43:26 +0300

    adding posts to the site + displaying full text

--- src/main/java/com/exampleblog/firstblog/controllers/BlogController.java
+++ src/main/java/com/exampleblog/firstblog/controllers/BlogController.java
@@ -1,9 +1,15 @@
 package com.exampleblog.firstblog.controllers;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.exampleblog.firstblog.models.Post;
 import com.exampleblog.firstblog.repositories.PostRepository;
@@ -19,6 +25,31 @@ public class BlogController {
     Iterable<Post> posts = postRepository.findAll();
 		model.addAttribute("posts", posts);
     return "blog-main";
+  }
+
+  @GetMapping("/blog/add")
+  public String blogAdd(Model model) {
+    return "blog-add";
+  }
+
+  @PostMapping("/blog/add")
+  public String blogPostAdd(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
+    Post post = new Post(title, anons, full_text);
+    postRepository.save(post);
+    return "redirect:/blog";
+  }
+
+  @GetMapping("/blog/{id}")
+  public String blogDetails(@PathVariable(value = "id") long id, Model model) {
+    if(!postRepository.existsById(id)) {
+      return "redirect:/blog";
+    }
+    
+    Optional<Post> post = postRepository.findById(id);
+    ArrayList<Post> result = new ArrayList<>();
+    post.ifPresent(result::add);
+    model.addAttribute("post", result);
+    return "blog-details";
   }
 
 }
