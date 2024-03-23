910afddef3310b07564169bc5593ae8e1ac74511
Анастасия Пуха <pukha_an@mail.ru>
2024-03-23 13:39:06 +0300

    added sending emails

--- src/main/java/com/exampleblog/firstblog/controllers/RegistrationController.java
+++ src/main/java/com/exampleblog/firstblog/controllers/RegistrationController.java
@@ -1,13 +1,19 @@
 package com.exampleblog.firstblog.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.exampleblog.firstblog.services.MailSender;
+
 @Controller
 public class RegistrationController {
+
+  @Autowired
+  private MailSender mailSender;
     
   @GetMapping("/registration")
   public String blogMain(Model model) {
@@ -18,6 +24,7 @@ public class RegistrationController {
   public String blogPostAdd(@RequestParam String inputEmail, @RequestParam String inputPassword, Model model) {
     model.addAttribute("email", inputEmail);
     model.addAttribute("password", inputPassword);
+    mailSender.send(inputEmail, "Test message", "Hello!\n" + "Your text: " + inputPassword);
     return "my-page";
   }
 
